6478d6ef55a6279cd791349e088ae3789dd3acc0
WallissonDev <wallissonjob@hotmail.com>
2026-08-15 08:52:15 -0300

    add SVG assets for hotel, lunch, and remove button; update index.html to include new items and styles; enhance script.js with expense input handling; modify global and refundcalls CSS for improved styling

---
+++ script.js
@@ -0,0 +1,5 @@
+const expenseTitleInput = document.getElementById('expense-title');
+const expenseCategorySelect = document.getElementById('expense-category');
+const expenseValueInput = document.getElementById('expense-value');
+const expenseSubmitButton = document.getElementById('expense-submit');
+
